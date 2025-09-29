089409898e7de38616109df4efc40570c31c254b
Riya Kamboj <kambojriya1504@gmail.com>
2025-09-29 14:05:19 +0530

    Add files via upload

---
+++ vid67/script.js
@@ -0,0 +1,5 @@
+console.log("hello ria!!")
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
---
+++ vid68/script.js
@@ -0,0 +1,16 @@
+console.log("ria")
+
+/* let boxes = document.getElementsByClassName("box")
+console.log(boxes)
+
+boxes[2].style.backgroundColor = "red" */
+
+/* document.getElementById("red").style.backgroundColor = "red" */
+
+/* document.querySelector(".box").style.backgroundColor = "green"; */
+
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "green";
+})
